5107a30fc847afd0c595374e7ef4242dd2f6f26f
iweb student 001 <iwebstudent001@creativemedia.space>
2019-07-05 14:19:17 -1000

    added blog post and navicon

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var myMenuButton = document.querySelector('.menu-button');
     var myMainMenu = document.querySelector('header .site-nav');
-    //console.log(myMainMenu);
     myMenuButton.addEventListener('click', function () {
-        //console.log("Hey Mom, I'm Clicked!")
         myMainMenu.classList.toggle('expanded');
     });
 
